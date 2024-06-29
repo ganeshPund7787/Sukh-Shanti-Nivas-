b07fcbaf851ec01b16ace0e1fff4166f0b764455
Ganesh Pund <ganupund7787@gmail.com>
2024-06-28 20:02:33 -0700

    fontend create fetch api

---
+++ frontend/src/API_Calls/fetchMyHotel.ts
@@ -0,0 +1,21 @@
+import { API_URL } from "../main";
+import { Hoteltype } from "../../../backend/src/models/hotel.model";
+
+const fetchMyHotel = async (): Promise<Hoteltype[]> => {
+  try {
+    const res = await fetch(`${API_URL}/api/my-hotels`, {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (!res.ok) {
+      throw new Error("faild to fetch hotels");
+    }
+    return res.json();
+  } catch (error: any) {
+    console.log(`Error while fetchMyHotel : ${error.message}`);
+    return [];
+  }
+};
+
+export default fetchMyHotel;
