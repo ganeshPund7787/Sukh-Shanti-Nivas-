f78c13c63f2a9e37c76427629aef05d0ac0d733c
Ganesh Pund <ganupund7787@gmail.com>
2024-06-30 21:39:30 -0700

    create context for search hotel

---
+++ frontend/src/context/SearchContext.tsx
@@ -0,0 +1,71 @@
+type SearchContext = {
+  destination: string;
+  checkIn: Date;
+  checkOut: Date;
+  adultCount: number;
+  childCount: number;
+  hotelId: string;
+  saveSearchValues: (
+    destination: string,
+    checkIn: Date,
+    checkOut: Date,
+    adultCount: number,
+    childCount: number
+  ) => void;
+};
+
+import { createContext, useContext, useState } from "react";
+const SearchContext = createContext<SearchContext | undefined>(undefined);
+
+type SearchContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const seachContextProvider = ({
+  children,
+}: SearchContextProviderProps) => {
+  const [destination, setDestination] = useState<string>("");
+  const [checkIn, setCheckin] = useState<Date>(new Date());
+  const [checkOut, setCheckOut] = useState<Date>(new Date());
+  const [adultCount, setAdultCount] = useState<number>(1);
+  const [childCount, setChildCount] = useState<number>(0);
+  const [hotelId, setHotelId] = useState<string>("");
+
+  const saveSearchValues = (
+    destination: string,
+    checkIn: Date,
+    checkOut: Date,
+    adultCount: number,
+    childCount: number,
+    hotelId?: string
+  ) => {
+    setDestination(destination);
+    setCheckin(checkIn);
+    setCheckOut(checkOut);
+    setAdultCount(adultCount);
+    setChildCount(childCount);
+    if (hotelId) {
+      setHotelId(hotelId);
+    }
+  };
+  return (
+    <SearchContext.Provider
+      value={{
+        destination,
+        checkIn,
+        checkOut,
+        adultCount,
+        childCount,
+        hotelId,
+        saveSearchValues,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearchContext = () => {
+    return useContext(SearchContext) as string
+}
+
